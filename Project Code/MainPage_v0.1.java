a57a01b77e6a495ca4585d3cfcf0399195cbf7fd
miskos7 <66915092+miskos7@users.noreply.github.com>
2023-04-25 16:35:11 +0300

    Add files via upload

---
+++ Project Code/MainPage_v0.1.java
@@ -0,0 +1,209 @@
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.concurrent.TimeUnit;
+
+
+
+
+
+public class MainPage extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainPage frame = new MainPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+public int width;
+	
+	public MainPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 501, 642);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(100, 100, 100, 100));
+		panel.setBackground(Color.WHITE);
+		panel.setForeground(Color.WHITE);
+		panel.setBounds(10, 10, 467, 585);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				width=400;
+				System.out.println(width);
+			}
+		});
+		Image img_1 = new ImageIcon(this.getClass().getResource("/menu.png")).getImage();
+		btnNewButton.setIcon(new ImageIcon(img_1));
+		btnNewButton.setBorder(BorderFactory.createEmptyBorder(4,4,4,4));
+		btnNewButton.setContentAreaFilled(false);
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setBackground(Color.WHITE);
+		btnNewButton.setBounds(10, 22, 50, 50);
+		panel.add(btnNewButton);
+		
+		System.out.println(width);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_1.setForeground(new Color(0, 0, 0));
+		panel_1.setBackground(new Color(192, 192, 192));
+		panel_1.setBounds(0, 22,0, 563);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JButton btnNewButton_4 = new JButton("Τα ραντεβού μου");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_4.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		btnNewButton_4.setContentAreaFilled(false);
+		btnNewButton_4.setBounds(39, 62, 290, 50);
+		panel_1.add(btnNewButton_4);
+		
+		JButton btnNewButton_4_1 = new JButton("Αιτήσεις");
+		btnNewButton_4_1.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		btnNewButton_4_1.setContentAreaFilled(false);
+		btnNewButton_4_1.setBounds(39, 122, 290, 50);
+		panel_1.add(btnNewButton_4_1);
+		
+		JButton btnNewButton_4_2 = new JButton("Ιστορτικό");
+		btnNewButton_4_2.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		btnNewButton_4_2.setContentAreaFilled(false);
+		btnNewButton_4_2.setBounds(39, 182, 290, 50);
+		panel_1.add(btnNewButton_4_2);
+		
+		JButton btnNewButton_4_3 = new JButton("Προφίλ");
+		btnNewButton_4_3.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		btnNewButton_4_3.setContentAreaFilled(false);
+		btnNewButton_4_3.setBounds(39, 242, 290, 50);
+		panel_1.add(btnNewButton_4_3);
+		
+		JButton btnNewButton_4_4 = new JButton("Προσωπικά Δεδομένα");
+		btnNewButton_4_4.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		btnNewButton_4_4.setContentAreaFilled(false);
+		btnNewButton_4_4.setBounds(39, 302, 290, 50);
+		panel_1.add(btnNewButton_4_4);
+		
+		JButton btnNewButton_4_5 = new JButton("Βοήθεια");
+		btnNewButton_4_5.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		btnNewButton_4_5.setContentAreaFilled(false);
+		btnNewButton_4_5.setBounds(39, 362, 290, 50);
+		panel_1.add(btnNewButton_4_5);
+		
+		JButton btnNewButton_4_6 = new JButton("Ρυθμίσεις");
+		btnNewButton_4_6.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		btnNewButton_4_6.setContentAreaFilled(false);
+		btnNewButton_4_6.setBounds(39, 422, 290, 50);
+		panel_1.add(btnNewButton_4_6);
+		
+		JButton btnNewButton_4_6_1 = new JButton("Αποσύνδεση");
+		btnNewButton_4_6_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		btnNewButton_4_6_1.setBorder(BorderFactory.createEmptyBorder(4,4,4,4));
+		btnNewButton_4_6_1.setContentAreaFilled(false);
+		btnNewButton_4_6_1.setForeground(new Color(0, 128, 255));
+		btnNewButton_4_6_1.setBackground(Color.WHITE);
+		btnNewButton_4_6_1.setBounds(0, 516, 133, 36);
+		panel_1.add(btnNewButton_4_6_1);
+		
+		JButton btnNewButton_1 = new JButton("");
+		Image img_2 = new ImageIcon(this.getClass().getResource("/profile.png")).getImage();
+		btnNewButton_1.setIcon(new ImageIcon(img_2));
+		btnNewButton_1.setBorder(BorderFactory.createEmptyBorder(4,4,4,4));
+		btnNewButton_1.setContentAreaFilled(false);
+		btnNewButton_1.setForeground(Color.WHITE);
+		btnNewButton_1.setBackground(Color.WHITE);
+		btnNewButton_1.setBounds(347, 22, 50, 50);
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_1_1 = new JButton("");
+		btnNewButton_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		Image img_3 = new ImageIcon(this.getClass().getResource("/settings.png")).getImage();
+		btnNewButton_1_1.setIcon(new ImageIcon(img_3));
+		btnNewButton_1_1.setBorder(BorderFactory.createEmptyBorder(4,4,4,4));
+		btnNewButton_1_1.setContentAreaFilled(false);
+		btnNewButton_1_1.setForeground(Color.WHITE);
+		btnNewButton_1_1.setBackground(Color.WHITE);
+		btnNewButton_1_1.setBounds(407, 22, 50, 50);
+		panel.add(btnNewButton_1_1);
+	
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		Image img_4 = new ImageIcon(this.getClass().getResource("/bankapp1.png")).getImage();
+		lblNewLabel.setIcon(new ImageIcon(img_4));
+		lblNewLabel.setBounds(171, 22, 95, 113);
+		panel.add(lblNewLabel);
+		
+		
+		String s1[] = {"-Επιλογή Τράπεζας-", "Τράπεζα Πειραιώς", "Εθνική Τράπεζα", "Eurobank", "AlphaBank" };
+		JComboBox comboBox = new JComboBox(s1);
+		comboBox.setBounds(135, 220, 184, 32);
+		panel.add(comboBox);
+		
+		
+		String s2[] = {"-Επιλογή Καταστήματος-" };
+		JComboBox comboBox_1 = new JComboBox(s2);
+		comboBox_1.setBounds(135, 272, 184, 32);
+		panel.add(comboBox_1);
+		
+		textField = new JTextField();
+		textField.setBounds(135, 178, 184, 32);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		Image img = new ImageIcon(this.getClass().getResource("/map.png")).getImage();
+		lblNewLabel_1.setIcon(new ImageIcon(img));
+		lblNewLabel_1.setBounds(10, 314, 447, 214);
+		panel.add(lblNewLabel_1);
+		
+		JButton btnNewButton_2 = new JButton("GO");
+		btnNewButton_2.setForeground(Color.BLACK);
+		btnNewButton_2.setBackground(SystemColor.textHighlight);
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnNewButton_2.setBounds(152, 538, 143, 37);
+		panel.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		Image img_5 = new ImageIcon(this.getClass().getResource("/search.png")).getImage();
+		btnNewButton_3.setIcon(new ImageIcon(img_5));
+		btnNewButton_3.setBorder(BorderFactory.createEmptyBorder(4,4,4,4));
+		btnNewButton_3.setContentAreaFilled(false);
+		btnNewButton_3.setForeground(Color.WHITE);
+		btnNewButton_3.setBackground(Color.WHITE);
+		btnNewButton_3.setBounds(329, 168, 50, 50);
+		panel.add(btnNewButton_3);
+	
+	}
+}
